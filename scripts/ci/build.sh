000133e95b13127c8a73fa17593d8e43a4c98fb6
Haochen Tong <i@hexchain.org>
2025-01-21 01:13:46 +0800

    unset TARGET_ARCH as it affects at least zstd and openldap

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -60,6 +60,12 @@ LOGS_FOLDER=${BUILD_LOGS_FOLDER:-./logs}
 
 mkdir -p $LOGS_FOLDER
 
+# ia32, x64, armv7, etc
+target_arch=${TARGET_ARCH:-"x64"}
+# the build system of some dependencies (e.g. zstd and openldap) seems to read
+# and use this variable, so unset it here to avoid making them fail
+unset TARGET_ARCH
+
 # The following two, libunistring and libidn2, are only necessary if building libcurl >= 7.53
 # However we are going to build then anyway, they are not that slow to build.
 
@@ -305,8 +311,6 @@ else
 fi
 
 target=`echo $target | sed 's/^v//'`
-# ia32, x64, armv7, etc
-target_arch=${TARGET_ARCH:-"x64"}
 
 NODE_LIBCURL_CPP_STD=${NODE_LIBCURL_CPP_STD:-$(node $curr_dirname/../cpp-std.js)}
 
